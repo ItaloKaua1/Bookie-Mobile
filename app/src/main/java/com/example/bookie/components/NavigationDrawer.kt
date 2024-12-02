d735a4b6d301308079aedc8997a43feb2306bf9b
LucasLevyOB <oliveiralucaslevy@gmail.com>
2024-12-02 08:07:22 -0300

    configuracao do projeto e esboco dos components iniciais

---
+++ app/src/main/java/com/example/bookie/components/NavigationDrawer.kt
@@ -0,0 +1,84 @@
+package com.example.bookie.components
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.outlined.AccountCircle
+import androidx.compose.material.icons.outlined.Edit
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.Info
+import androidx.compose.material.icons.outlined.Settings
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.NavigationDrawerItem
+import androidx.compose.material3.NavigationDrawerItemDefaults
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.rememberDrawerState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.launch
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview
+@Composable
+fun NavigationDrawer() {
+    val drawerState = rememberDrawerState(initialValue = DrawerValue.Open)
+    val scope = rememberCoroutineScope()
+    ModalNavigationDrawer(
+        drawerState = drawerState,
+        drawerContent = {
+            ModalDrawerSheet {
+                Row {
+                    Icon(
+                        imageVector = Icons.Outlined.AccountCircle,
+                        contentDescription = "Imagem de Usuário"
+                    )
+                    Text(text = "teste")
+                }
+            }
+        },
+    ) {
+        Scaffold(
+            topBar = {
+                TopBar(
+                    onOpenDrawer = {
+                        scope.launch {
+                            drawerState.apply {
+                                if (isOpen)
+                                    close()
+                                else
+                                    open()
+                            }
+                        }
+                    }
+                )
+            }
+        ) { contentPadding ->
+            // Screen content
+            Modifier.padding(contentPadding)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/bookie/components/TopBar.kt
@@ -0,0 +1,79 @@
+package com.example.bookie.components
+
+import android.util.Log
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.bookie.R
+import com.example.bookie.ui.theme.BookieTheme
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopBar(
+    onOpenDrawer: () -> Unit
+) {
+    TopAppBar(
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer,
+            titleContentColor = MaterialTheme.colorScheme.primary,
+        ),
+        title = {
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(
+                    modifier =
+                        Modifier.size(48.dp).fillMaxWidth()
+                            .padding(end = 8.dp)
+                            .clickable {
+                                onOpenDrawer()
+                            },
+                    imageVector = Icons.Filled.AccountCircle,
+                    contentDescription = "profile picture",
+                )
+                Text("Oi, Olívia!")
+            }
+        },
+        actions = {
+            IconButton(onClick = {}) {
+                Icon(
+                    modifier = Modifier.size(48.dp).fillMaxWidth().padding(end = 8.dp),
+                    painter = painterResource(R.drawable.ic_decobrir_livros),
+                    contentDescription = "Descobrir Livros",
+                )
+            }
+            IconButton(onClick = {}) {
+                Icon(
+                    modifier = Modifier.size(48.dp).fillMaxWidth().padding(end = 8.dp),
+                    painter = painterResource(R.drawable.ic_local_library),
+                    contentDescription = "Minha Biblioteca",
+                )
+            }
+        }
+    )
+}
+
+@Preview
+@Composable
+fun TopBarPreview() {
+    BookieTheme {
+        TopBar(onOpenDrawer = { })
+    }
+}
