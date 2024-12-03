6a2ba7c1ce8ca3036c9bcede39e4f30a21cf615c
LucasLevyOB <oliveiralucaslevy@gmail.com>
2024-12-03 09:09:24 -0300

    feat: bottom-bar e ajuste na navigation bar

---
+++ app/src/main/java/com/example/bookie/components/BottomBar.kt
@@ -0,0 +1,69 @@
+package com.example.bookie.components
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Add
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.Notifications
+import androidx.compose.material.icons.outlined.Search
+import androidx.compose.material.icons.outlined.Send
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.bookie.models.NavigationItem
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview
+@Composable
+fun BottomBar() {
+    val navigationItems = listOf(
+        NavigationItem(
+            title = "home",
+            icon = Icons.Outlined.Home
+        ),
+        NavigationItem(
+            title = "busca",
+            icon = Icons.Outlined.Search
+        ),
+        NavigationItem(
+            title = "criar",
+            icon = Icons.Outlined.Add
+        ),
+        NavigationItem(
+            title = "chat",
+            icon = Icons.Outlined.Send
+        ),
+        NavigationItem(
+            title = "notificações",
+            icon = Icons.Outlined.Notifications
+        ),
+    )
+
+    BottomAppBar (
+        actions = {
+            navigationItems.forEachIndexed { index, item ->
+                TextButton(
+                    onClick = { /* do something */ },
+                    colors = ButtonDefaults.buttonColors(contentColor = Color.Gray, containerColor = Color.Transparent)
+                ) {
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Icon(
+                            item.icon,
+                            contentDescription = item.title
+                        )
+                        Text(text = item.title)
+                    }
+                }
+            }
+        },
+    )
+}
--- app/src/main/java/com/example/bookie/components/NavigationDrawer.kt
+++ app/src/main/java/com/example/bookie/components/NavigationDrawer.kt
@@ -1,44 +1,39 @@
 package com.example.bookie.components
 
-import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Edit
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Info
-import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.outlined.AccountBox
 import androidx.compose.material.icons.outlined.AccountCircle
-import androidx.compose.material.icons.outlined.Edit
-import androidx.compose.material.icons.outlined.Home
-import androidx.compose.material.icons.outlined.Info
+import androidx.compose.material.icons.outlined.Done
+import androidx.compose.material.icons.outlined.ExitToApp
+import androidx.compose.material.icons.outlined.List
+import androidx.compose.material.icons.outlined.Person
+import androidx.compose.material.icons.outlined.Refresh
 import androidx.compose.material.icons.outlined.Settings
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.ModalDrawerSheet
 import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.NavigationDrawerItem
-import androidx.compose.material3.NavigationDrawerItemDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.bookie.models.NavigationItem
 import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -47,16 +42,88 @@ import kotlinx.coroutines.launch
 fun NavigationDrawer() {
     val drawerState = rememberDrawerState(initialValue = DrawerValue.Open)
     val scope = rememberCoroutineScope()
+
+    val navigationItems = listOf(
+        NavigationItem(
+            title = "meu perfil",
+            icon = Icons.Outlined.Person
+        ),
+        NavigationItem(
+            title = "configurações",
+            icon = Icons.Outlined.Settings
+        ),
+        NavigationItem(
+            title = "solicitações de amizade",
+            icon = Icons.Outlined.AccountBox
+        ),
+        NavigationItem(
+            title = "relatórios de leitura",
+            icon = Icons.Outlined.List
+        ),
+        NavigationItem(
+            title = "salvos",
+            icon = Icons.Outlined.Done
+        ),
+        NavigationItem(
+            title = "trocar livros",
+            icon = Icons.Outlined.Refresh
+        ),
+        NavigationItem(
+            title = "sair",
+            icon = Icons.Outlined.ExitToApp
+        ),
+    )
+
     ModalNavigationDrawer(
         drawerState = drawerState,
         drawerContent = {
             ModalDrawerSheet {
-                Row {
+                Row (
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier.padding(top = 40.dp, start = 16.dp, end = 16.dp, bottom = 16.dp)
+                ) {
                     Icon(
+                        modifier = Modifier.size(48.dp).fillMaxWidth().padding(end = 8.dp),
                         imageVector = Icons.Outlined.AccountCircle,
                         contentDescription = "Imagem de Usuário"
                     )
-                    Text(text = "teste")
+                    Column {
+                        Text(text = "Olivia")
+                        Text(text = "@oliviarodri")
+                    }
+                }
+                Row (
+                    horizontalArrangement = Arrangement.spacedBy(24.dp),
+                    modifier = Modifier.padding(horizontal = 16.dp)
+                ) {
+                    Text(text = "19 livros")
+                    Text(text = "89 amigos")
+                }
+                HorizontalDivider(
+                    thickness = 1.dp,
+                    modifier = Modifier.padding(vertical = 24.dp)
+                )
+                navigationItems.forEachIndexed { index, item ->
+                    NavigationDrawerItem (
+                        label = { Text(text = item.title) },
+//                            selected = index == selectedItemIndex,
+                        selected = false,
+                        onClick = {
+                            //  navController.navigate(item.route)
+
+//                                selectedItemIndex = index
+//                                scope.launch {
+//                                    drawerState.close()
+//                                }
+                        },
+                        icon = {
+                            Icon(
+                                imageVector = item.icon,
+                                contentDescription = item.title
+                            )
+                        },
+                        modifier = Modifier.padding(vertical = 8.dp).height(24.dp)
+                    )
                 }
             }
         },
