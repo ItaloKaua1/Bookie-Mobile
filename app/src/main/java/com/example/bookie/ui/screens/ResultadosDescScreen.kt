2065b84d2041832155b24c43de00b6b6478f6470
ItaloKaua1 <italo.kaua.11@gmail.com>
2025-01-21 16:12:40 -0300

    adc titulo na tela

--- app/src/main/java/com/example/bookie/ui/screens/ResultadosDescScreen.kt
+++ app/src/main/java/com/example/bookie/ui/screens/ResultadosDescScreen.kt
@@ -2,20 +2,35 @@ package com.example.bookie.ui.screens
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.bookie.components.LayoutVariant
+import com.example.bookie.ui.theme.quaternary
 
 @Composable
 fun ResultadosDescScreen(navController: NavController){
 
     LayoutVariant(navController, "Descobrir", false) {
 
-        Column(modifier = Modifier.fillMaxSize()) {
+        Column(modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+        ) {
             Text(
-                "Resultados Aqui"
+                "Melhores resultados pra sua busca: ",
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold,
+                style = MaterialTheme.typography.titleLarge.copy(
+                    color = MaterialTheme.colorScheme.quaternary,
+                    fontWeight = FontWeight.Bold
+                )
             )
         }
     }
