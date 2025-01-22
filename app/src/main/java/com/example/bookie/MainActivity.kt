4de44c2c941e7e54830750e230c9ede95b34af10
ItaloKaua1 <italo.kaua.11@gmail.com>
2025-01-21 21:47:42 -0300

    mudancas main activity

--- app/src/main/java/com/example/bookie/MainActivity.kt
+++ app/src/main/java/com/example/bookie/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.example.bookie
 
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
@@ -11,6 +14,7 @@ import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.graphics.toArgb
+import androidx.core.content.ContextCompat
 import androidx.core.view.WindowInsetsControllerCompat
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
@@ -22,12 +26,77 @@ import com.example.bookie.ui.screens.*
 import com.example.bookie.ui.screens.CadastroScreens.CadastroScreen1
 import com.example.bookie.ui.screens.CadastroScreens.CadastroScreen2
 import com.example.bookie.ui.screens.CadastroScreens.CadastroScreen3
+import com.example.bookie.ui.screens.ConfiguracoesTela
+import com.example.bookie.ui.screens.DescobrirScreen
+import com.example.bookie.ui.screens.FeedScreen
+import com.example.bookie.ui.screens.ListarLivros
+import com.example.bookie.ui.screens.LoginScreen
+import com.example.bookie.ui.screens.MinhaEstante
+import com.example.bookie.ui.screens.ResultadosDescScreen
+import com.example.bookie.ui.screens.TelaLivro
+import com.example.bookie.ui.screens.TelaNotificacoes
+import com.example.bookie.ui.screens.TelaPerfil
+import com.example.bookie.ui.theme.BookieTheme
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.messaging.FirebaseMessaging
 
 class MainActivity : ComponentActivity() {
     private val configuracoesViewModel: ConfiguracoesViewModel by viewModels()
 
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission(),
+    ) { isGranted: Boolean ->
+        if (isGranted) {
+            // FCM SDK (and your app) can post notifications.
+        } else {
+            // TODO: Inform user that that your app will not show notifications.
+        }
+    }
+
+    private fun askNotificationPermission() {
+        // This is only necessary for API level >= 33 (TIRAMISU)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    android.Manifest.permission.POST_NOTIFICATIONS
+                ) == PackageManager.PERMISSION_GRANTED
+            ) {
+                // FCM SDK (and your app) can post notifications.
+            } else if (shouldShowRequestPermissionRationale(android.Manifest.permission.POST_NOTIFICATIONS)) {
+                // TODO: display an educational UI explaining to the user the features that will be enabled
+                //       by them granting the POST_NOTIFICATION permission. This UI should provide the user
+                //       "OK" and "No thanks" buttons. If the user selects "OK," directly request the permission.
+                //       If the user selects "No thanks," allow the user to continue without notifications.
+            } else {
+                // Directly ask for the permission
+                requestPermissionLauncher.launch(android.Manifest.permission.POST_NOTIFICATIONS)
+            }
+        }
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.Theme_Bookie)
         super.onCreate(savedInstanceState)
+
+        askNotificationPermission()
+
+        FirebaseMessaging.getInstance().isAutoInitEnabled = true
+
+        val TAG = "token-teste"
+
+        FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
+            if (!task.isSuccessful) {
+                Log.w(TAG, "Fetching FCM registration token failed", task.exception)
+                return@OnCompleteListener
+            }
+
+            // Get new FCM registration token
+            val token = task.result
+
+            Log.d(TAG, "token de teste: $token")
+//            Toast.makeText(baseContext, "token de teste: $token", Toast.LENGTH_SHORT).show()
+        })
 
         setContent {
             val navController = rememberNavController()
@@ -55,6 +124,19 @@ class MainActivity : ComponentActivity() {
                     composable("telaNotificacoes") { TelaNotificacoes(navController) }
                     composable("configuracoesTela") {
                         ConfiguracoesTela(navController = navController, viewModel = configuracoesViewModel)
+                    }
+                    composable(
+                        route = "telaLivro/{id}/{estante}",
+                        arguments = listOf(
+                            navArgument(name = "id") { type = NavType.StringType },
+                            navArgument(name = "estante") { type = NavType.BoolType }
+                        )
+                    ) { backstackEntry ->
+                        val idLivro = backstackEntry.arguments?.getString("id")
+                        val estante = backstackEntry.arguments?.getBoolean("estante")
+                        if (idLivro != null) {
+                            TelaLivro(navController, id = idLivro, estante = estante)
+                        }
                     }
                     composable("descobrirLivro") { DescobrirScreen(navController) }
                     composable("resultadosDescobrir") { ResultadosDescScreen(navController) }
